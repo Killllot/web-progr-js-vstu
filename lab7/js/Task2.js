106d1df00e9272e62c0aaa6ef3eaa9121ecad49b
kirill <fillontino@gmail.com>
2022-10-30 20:02:53 +0300

    Добавил 7 b 8

--- lab7/js/Task2.js
+++ lab7/js/Task2.js
@@ -1,13 +1,55 @@
 $(function () {
-    onload.per
+
     $('#partner_data').hide();
     $('#warning').hide();
 
     $('#fio').focus();
+    $("#married").prop('checked', false);
+    $("#single").prop('checked', false);
 
-    $('married',"single").attr('disabled', true);
 
-    $('#send_data').click(function () {
+    $("#radiob :radio").on("click", function (e) {
+        console.clear();
+        console.log(this.checked);
+        e.preventDefault();
+        var btn = $(this).val();
+        if(btn === 'yes') {
+            $("#partner_data").show();
+        }
+        else {
+            $("#partner_data").hide();
+        }
+        setTimeout(
+            () => $(this).prop("checked", !this.checked).trigger("change")
+        );
+    });
+
+    $("#send_data").click(function (e) {
+        var fieldFIO = $('#fio').val();
+        var fieldPhone = $('#tel input').val();
+        var fieldEmail = $('#email input').val();
+        var fieldPost = $('#post input').val();
+        var fieldMarr = $('#partner_data input').val();
+
+
+
+        if ($('#married').attr('checked')) {
+            if(fieldFIO ==="" || fieldPhone ==="" || fieldEmail ==="" || fieldPost ==="" || fieldMarr ==="") {
+                console.log(fieldFIO);
+                console.log(fieldPhone);
+                console.log(fieldEmail);
+                console.log(fieldPost);
+                console.log(fieldMarr);
+                alert("Не все поля заполнены!");
+                return false;
+            }
+        }
+        else
+            if(fieldFIO ==="" || fieldPhone ==="" || fieldEmail ==="" || fieldPost ==="") {
+                alert("Не все поля заполнены!");
+                return false;
+            }
+
 
     })
 });
---
+++ lab8/js/Task1.js
@@ -0,0 +1,115 @@
+$(function () {
+    function Train (num, p_naz, v_otpr) {
+        this.num = num;
+        this.p_naz = p_naz;
+        this.v_otpr = v_otpr;
+    }
+    Train.prototype = {
+        constructor : Train,
+        PrintTrain: function() {
+            var tr='<tr><td>'+this.num+'</td><td>'+this.p_naz+
+                '</td><td>'+this.v_otpr+'</td></tr>';
+            return tr;
+        },
+        PrintRec: function() {
+            var tr = "<br>Номер поезда: "+this.num+"	Пункт назначения: "+this.p_naz+"	Время отправления :"+this.v_otpr;
+            return tr;
+        },
+        Search: function (na) {
+          if (na.num === this.num) {
+              this.PrintTrain(na);
+          }
+        }
+    };
+
+    var t = new Train(12, 'Казань', '12:30');
+    var arrPr = [];
+    arrPr.push(t);
+
+    $('#input_train').hide();
+    $('#delete_train').hide();
+    $('#search_train').hide();
+    $('#output_table').hide();
+
+    $('#action').change(function()   {
+        var obj = $(this).val();
+        if (obj === 'input') {
+            $('#output_table').hide();
+            $('#input_train').show();
+        }
+        if (obj === 'blank') {
+            $('#output_table').hide();
+            $('#input_train').hide();
+        }
+        if (obj === 'output') {
+            $('#output_table').show();
+            $('#input_train').hide();
+
+            let msHtml = "<table>";
+
+            arrPr.forEach( (tr) => {
+                msHtml += tr.PrintTrain(tr);
+            })
+            msHtml += "</table>"
+            $('#output_table').html(msHtml);
+        }
+        if (obj === 'search') {
+            $('#output_table').html("");
+            $('#input_train').hide();
+            $('#search_train').show();
+        }
+        if (obj === 'delete') {
+            $('#delete_train').show();
+            $('#input_train').hide();
+        }
+    });
+
+    $("#b_add").click(function (e) {
+        var fieldNum = $('#num input').val();
+        var fieldAdr = $('#address input').val();
+        var fieldTime = $('#time input').val();
+
+        if(fieldNum ==="" || fieldAdr ==="" || fieldTime ==="") {
+            console.log(fieldNum);
+            console.log(fieldAdr);
+            console.log(fieldTime);
+            alert("Не все поля заполнены!");
+            return false;
+        }
+        else {
+            var t = new Train(fieldNum, fieldAdr, fieldTime)
+            arrPr.push(t);
+            alert("Добавлено: " + t);
+            return false;
+        }
+
+
+    });
+
+    $("#s_find").click(function (e) {
+        var fieldNum = $('#num input').val();
+
+        if(fieldNum ==="") {
+            console.log(fieldNum);
+            alert("Не все поля заполнены!");
+            return false;
+        }
+        else {
+            let msHtml = "<table>";
+
+            arrPr.forEach( (tr) => {
+                msHtml += tr.search(fieldNum);
+            });
+
+            msHtml += "</table>"
+
+            $('#output_table').html(msHtml);
+
+
+            return false;
+        }
+
+
+    });
+
+});
